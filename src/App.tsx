c45384faf0fb820707025e2e06b48a0ab2f8cdcd
McAllen Fradejas <58442636+Somebody324@users.noreply.github.com>
2026-04-25 01:04:48 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-black">
+      <h1 className="text-3xl font-bold">
+        LuminAIte is Live 🚀
+      </h1>
+    </div>
+  );
+}
